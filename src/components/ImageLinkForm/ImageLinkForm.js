3c7194e19eeb3ac2ff393c7488f1390cb8d82050
RecodeExistence <recodeexistance@gmail.com>
2020-01-08 14:53:14 +0000

    fixed postitioning and responsive design.

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,14 +5,13 @@ import Camera from './vintagecamera.png';
 const ImageLinkForm = () => {
     return (
         <div className = "image-link-container">
-            <p className = "f3 tc">
-                {'Face Recognition tool;  Supply a picture and find a face!'}
+            <p className = "f3 tc" style = {{'font-size': '2em', 'font-family': 'wild river'}}>
+                {'Supply a picture and find a face!'}
             </p>
         
             <div className = 'pa4 br2 shadow-5 input-container center'>
                 <input className = "f4 pa2 w-70" type = "text" placeholder = "Paste Image Url Here..." />
                 <img className = "tc w-30 grow f4 link ph3 pv2 dib white cameraButton" src = { Camera } alt = "A vintage camera in place of a button"  onClick = {() => document.querySelector('.Tilt').setAttribute('id', 'clicked')}/>
-            
             </div>
         </div>  
     );
