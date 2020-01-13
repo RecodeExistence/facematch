809248154c02bc0ba80d6fa294417f647662e690
RecodeExistence <recodeexistance@gmail.com>
2020-01-13 17:04:29 +0000

    changed sizing and responsiveness

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = ({ onSubmitClicked, input }) => {
     return (
         <div className = 'ma4 mt0 center'>
         {onSubmitClicked}
-            <Tilt className = 'Tilt br2 ba b--white br4 shadow-2 tc pa4' options = {{ max : 30 }} style = {{height: '150px', width: '150px'}} >
+            <Tilt className = 'Tilt br2 ba b--white br4 shadow-2 tc pa4' options = {{ max : 30 }} style = {{height: '80%', width: '80%'}} >
                 <div className = 'Tilt-inner'>
                     <img src = {input} alt = 'logo of a face'/>
                 </div>
